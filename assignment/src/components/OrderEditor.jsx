302ffc7a1bcee6d3793f5aac08c21aed5ec447b2
tedpool <persisne@gmail.com>
2024-04-05 20:28:23 +0900

    feat: add day12 mission

--- assignment/src/components/OrderEditor.jsx
+++ assignment/src/components/OrderEditor.jsx
@@ -1,24 +1,22 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 
 export default function OrderEditor() {
-  const [menu, setMenu] = useState('');
-  const [address, setAddress] = useState('');
-  const [request, setRequest] = useState('');
-  
-  const handleOnMenu = (event) => {
-    setMenu(event.target.value);
-  }
+  const [order, setOrder] = useState({ menu: '', address: '', request: ''});
+  const addressInputRef = useRef(null);
 
-  const handleOnAddress = (event) => {
-    setAddress(event.target.value);
-  }
-
-  const handleOnRequest = (event) => {
-    setRequest(event.target.value);
+  const handleOnOrder = (event) => {
+    setOrder({
+      ...order,
+      [event.target.name]: event.target.value
+    })
   }
 
   const onSubmit = () => {
-    alert(`주문이 완료되었습니다. 메뉴: ${menu}, 주소: ${address}, 요청사항: ${request}`)
+    if (!order.address) {
+      addressInputRef.current.focus();
+      return;
+    }
+    alert(`주문이 완료되었습니다. 메뉴: ${order.menu}, 주소: ${order.address}, 요청사항: ${order.request}`)
   }
 
   return (
@@ -30,7 +28,7 @@ export default function OrderEditor() {
         <div style={{ marginBottom: 5, fontSize: 14 }}>
           메뉴 선택
         </div>
-        <select value={menu} onChange={handleOnMenu} style={{ width: 300, padding: 5 }}>
+        <select name='menu' value={order.menu} onChange={handleOnOrder} style={{ width: 300, padding: 5 }}>
           <option value={"족발"}>족발</option>
           <option value={"떡볶이"}>떡볶이</option>
           <option value={"아이스크림"}>아이스크림</option>
@@ -42,8 +40,10 @@ export default function OrderEditor() {
           배달 주소
         </div>
         <input
-          onChange={handleOnAddress}
-          value={address}
+          ref={addressInputRef}
+          name='address'
+          onChange={handleOnOrder}
+          value={order.address}
           style={{ width: 300, padding: 5 }}
           placeholder="주소) 서울특별시 xx동 .."
         />
@@ -53,8 +53,9 @@ export default function OrderEditor() {
           배달 요청사항
         </div>
         <textarea
-          onChange={handleOnRequest}
-          value={request}
+          name='request'
+          onChange={handleOnOrder}
+          value={order.request}
           style={{ width: 300, padding: 5 }}
           placeholder="배달 요청사항을 써 주세요..."
         />
