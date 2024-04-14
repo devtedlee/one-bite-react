69969ad79958d8186c63325e345d990ed90dc356
tedpool <persisne@gmail.com>
2024-04-14 22:23:25 +0900

    feat: add day 19 mission

--- final/src/App.jsx
+++ final/src/App.jsx
@@ -1,39 +1,94 @@
-import './App.css'
-import { Routes, Route, Link, useNavigate } from 'react-router-dom'
-import Home from './pages/Home'
-import New from './pages/New'
-import Diary from './pages/Diary'
-import Edit from './pages/Edit'
-import NotFound from './pages/NotFound'
+import "./App.css";
+import { useRef, useReducer, createContext } from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import New from "./pages/New";
+import Diary from "./pages/Diary";
+import Edit from "./pages/Edit";
+import NotFound from "./pages/NotFound";
 
-// 1. '/': 모든 일기를 조회하는 Home 페이지
-// 2. '/new': 새 일기를 작성하는 New 페이지
-// 3. '/diary : 일기를 조회하는 Diary 페이지
-function App() {
-  const nav = useNavigate();
-  const onClick = () => {
-    nav('/new');
-  };
+const mockData = [
+  {
+    id: 1,
+    createdDate: new Date().getTime(),
+    emotionId: 1,
+    content: "1번 일기 내용",
+  },
+  {
+    id: 2,
+    createdDate: new Date().getTime(),
+    emotionId: 2,
+    content: "2번 일기 내용",
+  },
+];
 
-  return (
-    <>
-      <div>
-
-        <Link to="/">Home</Link>
-        <Link to="/new">New</Link>
-        <Link to="/edit/:id">Edit</Link>
-        <Link to="/diary/:id">Diary</Link>
-      </div>
-      <button onClick={onClick}>New 페이지로 이동</button>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/new" element={<New />} />
-        <Route path="/edit/:id" element={<Edit />} />
-        <Route path="/diary/:id" element={<Diary />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </>
-  )
+function reducer(state, action) {
+  switch (action.type) {
+    case "ADD":
+      return [...state, action.diary];
+    case "EDIT":
+      return state.map((diary) =>
+        String(diary.id) === String(action.diary.id) ? action.diary : diary
+      );
+    case "DELETE":
+      return state.filter((diary) => String(diary.id) !== String(action.id));
+    default:
+      break;
+  }
+  return state;
 }
 
-export default App
+const DiaryStateContext = createContext();
+const DiaryDispatchContext = createContext();
+
+function App() {
+  const idRef = useRef(3);
+  const [data, dispatch] = useReducer(reducer, mockData);
+
+  const handleOnAddDiary = (createdDate, emotionId, content) => {
+    dispatch({
+      type: "ADD",
+      diary: {
+        id: idRef.current++,
+        createdDate,
+        emotionId,
+        content,
+      },
+    });
+  };
+
+  const handleOnEditDiary = (id, createdDate, emotionId, content) => {
+    dispatch({
+      type: "EDIT",
+      diary: {
+        id,
+        createdDate,
+        emotionId,
+        content,
+      },
+    });
+  };
+
+  const handleOnDeleteDiary = (id) => {
+    dispatch({ type: "DELETE", id });
+  };
+  return (
+    <>
+      <DiaryStateContext.Provider value={data}>
+        <DiaryDispatchContext.Provider
+          value={{ handleOnAddDiary, handleOnEditDiary, handleOnDeleteDiary }}
+        >
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/new" element={<New />} />
+            <Route path="/diary/:id" element={<Diary />} />
+            <Route path="/edit/:id" element={<Edit />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </DiaryDispatchContext.Provider>
+      </DiaryStateContext.Provider>
+    </>
+  );
+}
+
+export default App;
