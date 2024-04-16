7efca911fd414d5780d473f67110978ca00c552e
tedpool <persisne@gmail.com>
2024-04-17 00:27:11 +0900

    feat: add day 22 mission

--- final/src/components/Editor.jsx
+++ final/src/components/Editor.jsx
@@ -1,38 +1,27 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Button from "./Button";
 import "./Editor.css";
 import EmotionItem from "./EmotionItem";
 import { useNavigate } from "react-router-dom";
+import { emotionList } from "../utils/constants";
+import { getStringedDate } from "../utils/getStringedDate";
 
-const emotionList = [
-  { emotionId: 1, emotionName: "완전 좋음" },
-  { emotionId: 2, emotionName: "좋음" },
-  { emotionId: 3, emotionName: "그럭저럭" },
-  { emotionId: 4, emotionName: "나쁨" },
-  { emotionId: 5, emotionName: "완전 나쁨" },
-];
-
-const getStringedDate = (targetDate) => {
-  // 날짜 -> YYYY-MM-DD
-  const year = targetDate.getFullYear();
-  let month = targetDate.getMonth() + 1;
-  let date = targetDate.getDate();
-  if (month < 10) {
-    month = `0${month}`;
-  }
-  if (date < 10) {
-    date = `0${date}`;
-  }
-  return `${year}-${month}-${date}`;
-};
-
-function Editor({ onSubmit }) {
+function Editor({ initData, onSubmit }) {
   const nav = useNavigate();
   const [input, setInput] = useState({
     createdDate: new Date(),
     emotionId: 3,
     content: "",
   });
+
+  useEffect(() => {
+    if (initData) {
+      setInput({
+        ...initData,
+        createdDate: new Date(Number(initData.createdDate)),
+      });
+    }
+  }, [initData]);
 
   const handleOnChangeInput = (e) => {
     let { name, value } = e.target;
--- final/src/pages/Edit.jsx
+++ final/src/pages/Edit.jsx
@@ -1,11 +1,53 @@
-import { useParams } from "react-router-dom";
+import Header from "../components/Header";
+import Editor from "../components/Editor";
+import Button from "../components/Button";
+import { useNavigate, useParams } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import { DiaryDispatchContext, DiaryStateContext } from "../App";
+import useDiary from "../hooks/useDiary";
 
 function Edit() {
-  const { id } = useParams();
+  const params = useParams();
+  const { handleOnDeleteDiary, handleOnEditDiary } =
+    useContext(DiaryDispatchContext);
+  const nav = useNavigate();
+  const currentDiaryItem = useDiary(params.id);
+
+  const handleOnClickDelete = () => {
+    if (window.confirm("정말로 삭제하시겠습니까?")) {
+      handleOnDeleteDiary(params.id);
+      nav("/", { replace: true });
+    }
+  };
+
+  const handleOnSubmit = (input) => {
+    if (window.confirm("수정하시겠습니까?")) {
+      handleOnEditDiary(
+        params.id,
+        input.createdDate.getTime(),
+        input.emotionId,
+        input.content
+      );
+      nav("/", { replace: true });
+    }
+  };
 
   return (
-    <div>{id}번 일기 입니다</div>
-  )
+    <div>
+      <Header
+        title={"일기 수정하기"}
+        leftChild={<Button onClick={() => nav(-1)} text={"< 뒤로 가기"} />}
+        rightChild={
+          <Button
+            onClick={handleOnClickDelete}
+            text={"삭제하기"}
+            type={"NEGATIVE"}
+          />
+        }
+      />
+      <Editor initData={currentDiaryItem} onSubmit={handleOnSubmit} />
+    </div>
+  );
 }
 
-export default Edit
+export default Edit;
